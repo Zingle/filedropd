021a78992fafc086d6fac25ddd8e765df50afa93
Richard Remer <rich.remer@gmail.com>
2021-12-15 18:09:46 -0800

    web server

--- bin/filedropd.js
+++ bin/filedropd.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-import * as foo from "filedropd";
+import express from "express";
+import tlsopt from "tlsopt";
+
+const app = express();
+const server = tlsopt.createServerSync(app);
+
+server.listen(() => logServerInfo(server));
+
+function logServerInfo(server) {
+  const {address, family, port} = server.address();
+  const host = family === "IPv6" ? `[${address}]` : address;
+  console.info(`listening on ${host}:${port}`);
+}
